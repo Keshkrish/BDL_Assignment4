46a5dde0996e61bd1b367548534f72b7f61ca4b6
keshkrish <keshavakrishna2001@gmail.com>
2024-04-07 17:57:00 +0530

    Handle for the case where every value in a month is Nan

--- evaluate.py
+++ evaluate.py
@@ -11,6 +11,9 @@ extracted_averages=extracted_averages.sort_index()
 computed_averages_values=computed_averages.iloc[:,:].values.flatten() #extract the values and flatten, to compute a single r2 score
 extracted_averages_values=extracted_averages.iloc[:,:].values.flatten()
 
+valid_indices=~np.isnan(computed_averages_values)#Check if there are any NaN values that may have been obtained when if an entire month had NaN as daily values
+computed_averages_values=computed_averages_values[valid_indices] #Retain the elements without Nan values
+extracted_averages_values=extracted_averages_values[valid_indices] #Retain the elements where the corresponding elements in computed_averages_values is not Nan
 #print(computed_averages_values)
 #print(extracted_averages_values)
 r2=r2_score(computed_averages_values,extracted_averages_values) #Compute r2 score
